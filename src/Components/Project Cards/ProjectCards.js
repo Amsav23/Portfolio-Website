a4de64c425ec17a03629788597e5ef1b6ce0c52c
Amsav23 <130859471+Amsav23@users.noreply.github.com>
2024-04-30 17:46:16 -0400

    Update
    updated my projectCards file, added in another project, and updated the styling of My Projects page.

--- src/Components/Project Cards/ProjectCards.js
+++ src/Components/Project Cards/ProjectCards.js
@@ -1,10 +1,23 @@
 // Project data
 const projects = [
-    { title: "UniLife", 
-    description: "UniLife has been my biggest project so far.<br> It is a website for students living in the UK to filter through cities and student housing.<br> They have the freedom to filter through criteria such as pricing, location, number of bedrooms or bathrooms, and more.", 
-    image: "/assets/UniLife Screenshot.png", url: "project1.html" },
-    { title: "Rick and Morty", description: "A fun webpage to filter through episodes of the adult animated show, Rick and Morty.<br> Users can read details about each character, save their favorites, and even switch to using dark mode.", 
-    image: "/assets/Rick and Morty Screenshot.png", url: "project2.html" },
+    { 
+        title: "UniLife", 
+        description: "UniLife has been my biggest project so far.<br> It is a website for students living in the UK to filter through cities and student housing.<br> They have the freedom to filter through criteria such as pricing, location, number of bedrooms or bathrooms, and more.", 
+        image: "/assets/UniLifeScreenshot.png", 
+        url: "Projects/project1.html" 
+    },
+    { 
+        title: "Rick and Morty", 
+        description: "A fun webpage to filter through episodes of the adult animated show, Rick and Morty.<br> Users can read details about each character, save their favorites, and even switch to using dark mode.", 
+        image: "/assets/RickandMortyScreenshot.png", 
+        url: "Projects/project2.html" 
+    },
+    {
+        title: "Fake Store",
+        description: "This is a pretend store that allows users to choose between a variety of categories, as if they were shopping online. Users have the option of 'favoriting', and 'unfavoriting', as many items as they'd like. Each time an item is 'favorited', that data gets stored in the 'shopping cart'. ",
+        image: "/assets/FakeStore.png",
+        url: "#",
+    },
 ]
 
 // Function to create project elements
@@ -13,8 +26,8 @@ function createProjectElement(project) {
     projectElement.classList.add("project");
     projectElement.innerHTML = 
         `<h2>${project.title}</h2>
-        <p>${project.description}</p>
         <img src="${project.image}" alt="${project.title}">
+        <p>${project.description}</p>
         <a href="${project.url}" target="_blank">View Project</a>
     `;
     return projectElement;
