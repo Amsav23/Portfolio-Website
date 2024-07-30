cabd9ef5e6c633e4272f1adcaa59587f571bb952
Amyln <amsavarese23@gmail.com>
2024-07-29 20:18:12 -0400

    Update
    Consolidated everything into one landing page. Got rid of folders for About, Contact, Projects. Moved all styling to style.css

--- src/Components/Project Cards/ProjectCards.js
+++ src/Components/Project Cards/ProjectCards.js
@@ -3,19 +3,19 @@ const projects = [
     { 
         title: "UniLife", 
         description: "UniLife has been my biggest project so far.<br> It is a website for students living in the UK to filter through cities and student housing.<br> They have the freedom to filter through criteria such as pricing, location, number of bedrooms or bathrooms, and more.", 
-        image: "/assets/UniLifeScreenshot.png", 
+        image: "/Images/UniLifeScreenshot.png", 
         url: "Projects/project1.html" 
     },
     { 
         title: "Rick and Morty", 
         description: "A fun webpage to filter through episodes of the adult animated show, Rick and Morty.<br> Users can read details about each character, save their favorites, and even switch to using dark mode.", 
-        image: "/assets/RickandMortyScreenshot.png", 
+        image: "/Images/RickandMortyScreenshot.png", 
         url: "Projects/project2.html" 
     },
     {
         title: "Fake Store",
         description: "This is a pretend store that allows users to choose between a variety of categories, as if they were shopping online. Users have the option of 'favoriting', and 'unfavoriting', as many items as they'd like. Each time an item is 'favorited', that data gets stored in the 'shopping cart'. ",
-        image: "/assets/FakeStore.png",
+        image: "/Images/FakeStore.png",
         url: "#",
     },
 ]
@@ -28,8 +28,7 @@ function createProjectElement(project) {
         `<h2>${project.title}</h2>
         <img src="${project.image}" alt="${project.title}">
         <p>${project.description}</p>
-        <a href="${project.url}" target="_blank">View Project</a>
-    `;
+        <a href="${project.url}" target="_blank">View Project</a>`;
     return projectElement;
 }
 
