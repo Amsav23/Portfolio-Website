c19ed9b456b1f889ee839f7c2df735b3a9ced7c9
Amyln <amsavarese23@gmail.com>
2024-11-14 18:51:54 -0500

    Update
    Trying to get the Hamburger Menu to work. Still needs editing

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// HAMBURGER MENU
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu =  document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+})
+
+
+// CONTACT FORM
 (function() {
   emailjs.init('kgKwvTqUKVEEFh0Lt'); // 1. Initialize EmailJS with your user ID
 })();
